678b07c300963849390c6b80e18467a21b41e9de
DevAkshayRaj <61178420+DevAkshayRaj@users.noreply.github.com>
2021-02-20 00:24:10 +0530

    Add files via upload

---
+++ MainTest.java
@@ -0,0 +1,19 @@
+package second_session;
+
+public class MainTest {
+	/*Error: Main method is not static in class second_session.MainTest, please define the main method as:
+   public static void main(String[] args)*/
+	public  void main(String[] args) {
+		
+	}
+	/*Error: Main method is not public in class second_session.MainTest, please define the main method as:
+	   public static void main(String[] args)*/
+	static void main(String[] args) {
+			
+		}	
+	/*Error: Main method is not static in class second_session.MainTest, please define the main method as:
+	   public static void main(String[] args)*/
+	public static void main() {
+			
+		}
+}
